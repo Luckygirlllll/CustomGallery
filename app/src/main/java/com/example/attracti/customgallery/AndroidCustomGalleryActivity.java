aa4ddfd52ff72768ea685b21a2d08a2b0e08b3e2
Luckygirlllll <irynakanivets@gmail.com>
2016-05-18 15:38:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/attracti/customgallery/AndroidCustomGalleryActivity.java
@@ -0,0 +1,106 @@
+package com.example.attracti.customgallery;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by attracti on 5/18/16.
+ */
+public class AndroidCustomGalleryActivity extends Activity {
+    private int count;
+    private Bitmap[] thumbnails;
+    private boolean[] thumbnailsselection;
+    private String[] arrPath;
+    private ImageAdapter imageAdapter;
+    ArrayList<String> f = new ArrayList<String>();// list of file paths
+    File[] listFile;
+
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        getFromSdcard();
+        GridView imagegrid = (GridView) findViewById(R.id.PhoneImageGrid);
+        imageAdapter = new ImageAdapter();
+        imagegrid.setAdapter(imageAdapter);
+
+    }
+    public void getFromSdcard()
+    {
+//        File file= new File(android.os.Environment.getExternalStorageDirectory(),"CameraApp");
+     File file= new File(Environment.getExternalStorageDirectory()+"/Audio_Recorder_Picture","CameraApp");
+
+        if (file.isDirectory())
+        {
+            listFile = file.listFiles();
+
+
+            for (int i = 0; i < listFile.length; i++)
+            {
+
+                f.add(listFile[i].getAbsolutePath());
+
+            }
+        }
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private LayoutInflater mInflater;
+
+        public ImageAdapter() {
+            mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        public int getCount() {
+            return f.size();
+        }
+
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = mInflater.inflate(
+                        R.layout.galleryitem, null);
+                holder.imageview = (ImageView) convertView.findViewById(R.id.thumbImage);
+
+                convertView.setTag(holder);
+            }
+            else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+
+            Bitmap myBitmap = BitmapFactory.decodeFile(f.get(position));
+            holder.imageview.setImageBitmap(myBitmap);
+            return convertView;
+        }
+    }
+    class ViewHolder {
+        ImageView imageview;
+
+
+    }
+}
